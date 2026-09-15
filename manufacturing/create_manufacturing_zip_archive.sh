f4ac94e88ed21a49edff4864586e12195f5ac760
David Cussans <David.Cussans@bristol.ac.uk>
2026-09-15 15:39:13 +0100

    Updated BoM for Newbury (adding L9, C455a, C455b ). Created simple script to make Zip archive. Made frewsh Zip archive

---
+++ manufacturing/create_manufacturing_zip_archive.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+DATE=15sep26
+zip -rv dune_afc4-0-5_gerber_drill_bom_pnp_${DATE}.zip BOM/BOM_AFC_v4-0-5_DUNE_Newbury_F4578-REV3_20260909101506.xlsx Gerber NC\ Drill Pick\ Place/PnP_AFC_v4.0.5_DUNE.csv Report\ Board\ Stack
